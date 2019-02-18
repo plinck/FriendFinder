cd698033f00c75f46f8737584f332149728b700b
Paul Linck <paul.linck@gmail.com>
2019-02-18 17:42:49 -0500

    Added valid Images

--- app/data/FriendsData.js
+++ app/data/FriendsData.js
@@ -6,8 +6,8 @@ class FriendsData {
 
     fetchFriends() {
         this.friends = [{
-                "name": "Ahmed",
-                "photo_url": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+                "name": "Madonna",
+                "photo_url": "https://pmcvariety.files.wordpress.com/2018/12/rexfeatures_8773364av.jpg?w=1000&h=563&crop=1",
                 "answers": [
                     "5",
                     "1",
@@ -22,8 +22,8 @@ class FriendsData {
                 ]
             },
             {
-                "name": "Jacob Deming",
-                "photo_url": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+                "name": "Kate Bush",
+                "photo_url": "http://www.alwaysontherun.net/katebushtop7.jpg",
                 "answers": [
                     "4",
                     "2",
@@ -38,8 +38,8 @@ class FriendsData {
                 ]
             },
             {
-                "name": "Jeremiah Scanlon",
-                "photo_url": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+                "name": "Annie Lennox",
+                "photo_url": "https://i.pinimg.com/originals/2f/59/e3/2f59e31ae4559c04944cbac90dada75e.jpg",
                 "answers": [
                     "5",
                     "2",
@@ -54,8 +54,8 @@ class FriendsData {
                 ]
             },
             {
-                "name": "Louis T. Delia",
-                "photo_url": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+                "name": "Dana Perino",
+                "photo_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Dana_Perino_%2825238814459%29.jpg/440px-Dana_Perino_%2825238814459%29.jpg",
                 "answers": [
                     "3",
                     "3",
@@ -70,8 +70,8 @@ class FriendsData {
                 ]
             },
             {
-                "name": "Lou Ritter",
-                "photo_url": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+                "name": "Sarah Eisen",
+                "photo_url": "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2016/09/16/103946525-EISEN_S_-481_RGB_sat.240x240.jpg?v=1530023158",
                 "answers": [
                     "4",
                     "3",
@@ -86,8 +86,8 @@ class FriendsData {
                 ]
             },
             {
-                "name": "Jordan Biason",
-                "photo_url": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+                "name": "Morgan Brennan",
+                "photo_url": "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/02/27/104308096-BRENNAN_M-458_D_RGB.240x240.jpg?v=1488234122",
                 "answers": [
                     "4",
                     "4",
@@ -102,8 +102,8 @@ class FriendsData {
                 ]
             },
             {
-                "name": "bruce",
-                "photo_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Deftones_-_Rock_am_Ring_2016_-2016156214055_2016-06-04_Rock_am_Ring_-_Sven_-_1D_X_-_0078_-_DV3P9737_mod.jpg/450px-Deftones_-_Rock_am_Ring_2016_-2016156214055_2016-06-04_Rock_am_Ring_-_Sven_-_1D_X_-_0078_-_DV3P9737_mod.jpg",
+                "name": "Julia Boorstin",
+                "photo_url": "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2016/10/10/104006133-BOORSTIN_J_-174_RGB.240x240.jpg?v=1530023021",
                 "answers": [
                     "3",
                     "3",
@@ -118,8 +118,8 @@ class FriendsData {
                 ]
             },
             {
-                "name": "Joseph Cerankowski",
-                "photo_url": "http:",
+                "name": "Kelly Evans",
+                "photo_url": "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2016/10/10/104006180-EVANS_K_-983_RGB.240x240.jpg?v=1531137034",
                 "answers": [
                     "3",
                     "3",
@@ -134,8 +134,8 @@ class FriendsData {
                 ]
             },
             {
-                "name": "jsamson",
-                "photo_url": "ASasAS.PNG",
+                "name": "Becky Quick",
+                "photo_url": "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2016/10/10/104006275-QUICK_B_-337_RGB_sat.1910x1000.jpg",
                 "answers": [
                     "1",
                     "1",
